ec3e1bd7269468a331dfea07132bb6b6ffb67d53
Isabelle GAUTIER <isabelle.gautier@orange.fr>
2016-10-04 16:40:52 +0200

    1ere version

---
+++ js/documents.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('#myCarousel').carousel({
+    interval: 3000
+    })
+    
+    $('#myCarousel').on('slid.bs.carousel', function() {
+        //alert("slid");
+    });
+    
+    
+});
+
+
