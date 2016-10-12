720ee535ecc79c9cc45fbc9771de273371c3f53c
Alexis Phuong <alexis.hvp@gmail.com>
2016-10-12 11:13:55 +0200

    initialisation du fichier app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
- 
+(function(){
+    var app = angular.module('dojo', []);
+
+    app.controller('AccueilController',function(){
+
+
+    });
+
+    app.directive('header',function(){
+        return {
+            restrict : 'EA',
+            name : 'header',
+            templateUrl : 'header.html'
+        }
+    });
+
+
+
+})();
+
