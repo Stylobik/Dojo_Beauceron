610e53ca2aaa400839d089cdc4bddd9a9724a90e
matthieu rougeolle <matthieu.rougeolle@gmail.com>
2016-10-12 14:17:50 +0200

    modification de app.js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
 (function(){
     var app = angular.module('dojo', []);
 
-    app.controller('AccueilController',function(){
+    app.controller('dojoController',function(){
 
 
     });
 
-    app.directive('lien',function(){
+    app.directive('liens',function(){
         return {
             restrict : 'E',
-            name : 'lien',
-            templateUrl : '../../partials/common/header.html'
+            name : 'liens',
+            templateUrl : '../../partials/common/liens.html'
         }
     });
 
-    app.directive('nav',function(){
+    app.directive('navigation',function(){
         return {
             restrict : 'E',
-            name : 'nav',
-            templateUrl : '../../partials/common/nav.html'
+            name : 'navigation',
+            templateUrl : '../../partials/common/navigation.html'
         }
     });
 
@@ -26,7 +26,7 @@
         return {
             restrict : 'E',
             name : 'pied',
-            templateUrl: '../../partials/common/footer.html'
+            templateUrl: '../../partials/common/pied.html'
         }
     });
 
