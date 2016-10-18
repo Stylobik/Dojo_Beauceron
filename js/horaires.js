644ebe5c22e6834c955d5cda804f58d01e06b49f
Isabelle GAUTIER <isabelle.gautier@orange.fr>
2016-10-18 15:25:01 +0200

    fichier horaires.js

--- js/horaires.js
+++ js/horaires.js
@@ -58,6 +58,77 @@ var tab=[{
 	}],
 	jour : "mercredi",
 	dojo : [{
-		
-	}]
+		nom : "Mainvilliers",
+		horaire : [{
+			heures : "16h00 à 17h00"
+		},
+		{
+			heures : "17h00 à 18h00"
+		},
+		{
+			heures : "18h00 à 19h30"
+		}],
+		né : [{
+			naissance : "2009/2010"
+		},
+		{
+			naissance : "2007/2008"
+		},
+		{
+			naissance : "2002/2006"
+		}],
+		nom : "La Madeleine",
+		horaire : [{
+			heures : "15h00 à 15h45"
+		},
+		{
+			heures : "15h45 à 16h45"
+		}],
+		né : [{
+			naissance : "2011/2012"
+		},
+		{
+			naissance : "2007/2010"
+		}],
+	}],
+	jour : "jeudi",
+	dojo : [{
+		nom : "Fontaine la Guyon",
+		horaire : [{
+			heures : "16h45 à 17h30"
+		},
+		{
+			heures : "17h30 à 18h30"
+		}],
+		né : [{
+			naissance : "2012, 2011, 2010"	
+		},
+		{
+			naissance : "2009, 2008 et avant"
+		}],
+	}],
+	jour : "vendredi",
+	dojo : [{
+		nom : "Mainvilliers",
+		horaire : [{
+			heures : "20h00 à 21h30"
+		}],
+		né : [{
+			naissance : "2001 et avant"
+		}],
+		nom : "La Madeleine",
+		horaire : [{
+			heures : "17h30 à 19h00"
+		}],
+	}],
+	jour : "samedi",
+	dojo : [{
+		nom : "Mainvilliers",
+		horaire : [{
+			heures : "10h00 à 11h30"
+		}],
+		né : [{
+			naissance : "Jeunes ou adultes - Cours à thème"
+		}],
+	}],
 }];
