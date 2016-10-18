33b1e4ec3aebf2cd81944ca9b0f7233aea160567
Isabelle GAUTIER <isabelle.gautier@orange.fr>
2016-10-18 13:58:00 +0200

    ajout branche horaires

---
+++ js/horaires.js
@@ -0,0 +1,63 @@
+var tab=[{
+	jour : "lundi",
+	dojo :[{
+		nom : "Mainvilliers",
+		horaire : [{
+			heures : "18h15 à 19h30"
+		},
+		{
+			heures : "19h30 à 21h00"
+		}],
+		né : [{
+			naissance : "2002 à 2008"
+		},
+		{
+			naissance : "2001 et avant - Physique"
+		}]
+		nom : "La Madeleine",
+		horaire : [{
+			heures : "18h15 à 19h15"
+		},
+		{
+			heures : "19h15 à 20h30"
+		},
+		{
+			heures : "20h30 à 22h15"
+		}],
+		né : [{
+			naissance : "2007 à 2010"
+		},
+		{
+			naissance : "2002 à 2006"
+		},
+		{
+			naissance : "Taïso"
+		}],
+	}],
+	jour : "mardi",
+	dojo : [{
+		nom : "Mainvilliers",
+		horaire : [{
+			heures : "17h30 à 18h15"
+		},
+		{
+			heures : "18h15 à 19h30"
+		},
+		{
+			heures : "19h45 à 21h15"
+		}],
+		né : [{
+			naissance : "2012/2011"
+		},
+		{
+			naissance : "Minimes cadets"
+		},
+		{
+			naisssance : "2001 et avant - Technique"
+		}],
+	}],
+	jour : "mercredi",
+	dojo : [{
+		
+	}]
+}];
