68546a47de63652a71b742094b2a9d7bc6ed185a
matthieu rougeolle <matthieu.rougeolle@gmail.com>
2016-10-27 16:48:32 +0200

    ajout de modales sur le calendrier

--- js/app.js
+++ js/app.js
@@ -306,12 +306,12 @@
       /* event source that contains custom events on the scope */
 
       $scope.events = [
-        { title: 'All Day Event', start: new Date(y, m, 1) },
-        { title: 'Long Event', start: new Date(y, m, d - 5), end: new Date(y, m, d - 2) },
-        { id: 999, title: 'Repeating Event', start: new Date(y, m, d - 3, 16, 0), allDay: false },
-        { id: 999, title: 'Repeating Event', start: new Date(y, m, d + 4, 16, 0), allDay: false },
-        { title: 'Birthday Party', start: new Date(y, m, d + 1, 19, 0), end: new Date(y, m, d + 1, 22, 30), allDay: false },
-        { title: 'Click for Google', start: new Date(y, m, 28), end: new Date(y, m, 29), url: 'http://google.com/' }
+        { title: 'All Day Event', start: new Date(y, m, 1), description:'Un peu de blabla pour test', color: '#BA515C' },
+        { title: 'Long Event', start: new Date(y, m, d - 5), end: new Date(y, m, d - 2), description:'Un peu de blabla pour test', color:'#BA515C' },
+        { id: 999, title: 'Repeating Event', start: new Date(y, m, d - 3, 16, 0), allDay: false, description:'Un peu de blabla pour test', color: '#3A87AD' },
+        { id: 999, title: 'Repeating Event', start: new Date(y, m, d + 4, 16, 0), allDay: false, description:'Un peu de blabla pour test', color: '#3A87AD' },
+        { title: 'Birthday Party', start: new Date(y, m, d + 1, 19, 0), end: new Date(y, m, d + 1, 22, 30), allDay: false, description:'Un peu de blabla pour test', color: '#7A771D' },
+        { title: 'Click for Google', start: new Date(y, m, 28), end: new Date(y, m, 29), url: 'http://google.com/', description:'Un peu de blabla pour test', color: '#7A771D' }
       ];
 
       /* config object */
@@ -328,7 +328,14 @@
                   center: '',
                   right: 'today prev,next'
               },
-              eventRender: $scope.eventRender
+              eventRender: $scope.eventRender,
+
+              eventClick:  function(calEvent) {
+              $('#modalTitle').html(calEvent.title);
+              $('#modalBody').html(calEvent.description);
+              $('#fullCalModal').modal();
+              }
+
           }   
       };
 
